5070a8ab7df6f27ff21aebe455095c3408cc459c
Mouadelhiloufi <elmouad940@gmail.com>
2025-10-31 12:08:07 +0100

    refactor: update .gitignore, remove VSCode settings, and enhance styling and functionality for task management

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,13 @@ const container_card = document.querySelector(".container_card");
 let datee = document.querySelector("#inputDate")
 const textArea = document.querySelector("#taskText");
 let options = document.getElementById("select");
+let popUp = document.querySelector(".popUp");
+const formModif= document.getElementById("formPop");
+let dateModif = document.querySelector("#inputDateM").value
 
 
 const form = document.querySelector(".taskInput");
+
 
 
 
@@ -31,7 +35,7 @@ for (let i = 0; i < 7; i++) {
       
       <div class="card" id="p${i + 1}">
                         
-                                <p class="P_card${i+1}"> ${i + 1}</p>
+                                <p class="P_card${i + 1}"> ${i + 1}</p>
                             </div>
                             
                             `
@@ -41,88 +45,110 @@ for (let i = 0; i < 7; i++) {
 }
 
 
-form.addEventListener('submit', function (e) {
+btn.addEventListener('click', function (e) {
     e.preventDefault();
 
 
     let inputDate = document.getElementById("inputDate").value
     let txt_content = document.getElementById("taskText").value
-    let cards=document.querySelectorAll(".card");
+    let cards = document.querySelectorAll(".card");
     let heure = document.getElementById("hour").value;
-    
+
     //   let inputDay = document.getElementById("inputDay").value
 
-    
-   
-    
-    
+
+
+
+
     let regex = /^[A-Za-z "]+$/;
     let regex_hour = /^[0-9HhrR"]+$/;
-    if (!regex.test(txt_content)||inputDate>5) {
+    if (!regex.test(txt_content) || inputDate > 5) {
         Swal.fire({
             icon: "error",
             title: "Oops...",
             text: "Votre input value note valide!",
         });
 
-        
-        
-    }else if (!regex_hour.test(heure)){
-            Swal.fire({
+
+
+    } else if (!regex_hour.test(heure)) {
+        Swal.fire({
             icon: "error",
             title: "Oops...",
             text: "Votre input value note valide!",
         });
-            
-        }else { 
-        
-        
-        const containerDay=document.getElementById(`p${inputDate}`);
-        let P_card=document.createElement("p");
-        let dev=document.createElement("div");
-        dev.className="div_style"
-        
-        P_card.className="PCard";
+
+    } else {
+
+
+        const containerDay = document.getElementById(`p${inputDate}`);
+        let P_card = document.createElement("p");
+        let dev = document.createElement("div");
+        dev.className = "div_style"
+       let btnmodif = document.createElement("button");
+        P_card.className = "PCard";
         // let icon=document.createElement("i");
-        
+
         // icon.classList="fas fa-font-awesome"
         // P_card.appendChild("icon")
-        P_card.textContent=  txt_content;
-        
-        let p_hour=document.createElement("p")
+        P_card.textContent = txt_content;
+        btnmodif.textContent = "modifier";
+
+        let p_hour = document.createElement("p")
         p_hour.textContent = heure;
-        p_hour.className="pHour";
+        p_hour.className = "pHour";
         containerDay.appendChild(dev)
         dev.appendChild(p_hour);
-         dev.appendChild(P_card);
-
-
+        dev.appendChild(P_card);
+        dev.appendChild(btnmodif);
         
-        if (options.value==="VIP"){
+
+
+
+        if (options.value === "VIP") {
             console.log(options.value);
-              dev.style.backgroundColor="red";
-              
-        }
-        else if(options.value==="Aniversaire"){
-             
-            dev.style.backgroundColor="green";
+            dev.style.backgroundColor = "red";
 
         }
-        else if(options.value==="Standard"){
-            dev.style.backgroundColor="blue";
-                    }
+        else if (options.value === "Aniversaire") {
 
-        else if(options.value==="Group"){
-            dev.style.backgroundColor="yellow";
-                    }
+            dev.style.backgroundColor = "green";
+
+        }
+        else if (options.value === "Standard") {
+            dev.style.backgroundColor = "blue";
+        }
+
+        else if (options.value === "Group") {
+            dev.style.backgroundColor = "yellow";
+        }
 
 
 
-         dev.addEventListener('click', function(e) {
-            dev.remove();
-             // supprime l element
+        dev.addEventListener('dblclick', function (e) {
+            
+            e.currentTarget.remove(); // supprimer l element
+            popUp.style.display="none";
         });
-        
+        btnmodif.addEventListener('click',function(e){
+          
+            popUp.style.display="flex";
+
+            
+            
+            
+
+
+        });
+
+
+
+
+
+
+
+
+
 
 
 
@@ -132,7 +158,6 @@ form.addEventListener('submit', function (e) {
 
     }
 });
-    
 
 
 
@@ -140,16 +165,17 @@ form.addEventListener('submit', function (e) {
 
 
 
-    
-    
-    
-    
-    
 
-    
-    
-    
-    
+
+
+
+
+
+
+
+
+
+
 
 
 
